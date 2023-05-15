a2f0b0cb16b502ce0a549064ae393af25bec5a01
Trung Nguyen Thien <ngthientrung@gmail.com>
2023-05-16 00:22:45 +0700

    UPDATED

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
-cd libs
-
-mkdir geos && cd geos
-tar -xzvf ../geos-202305.tar.gz --strip-components=1
+mkdir  -p libs/geos && cd libs/geos
+tar -xzvf ../../zips/geos-202305.tar.gz --strip-components=1
+cd ../..
 mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release ..
 
-cd ..
