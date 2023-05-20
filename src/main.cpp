235c1d984a97b85a96de56a59c5d43560116a299
Trung Nguyen Thien <ngthientrung@gmail.com>
2023-05-20 18:05:56 +0700

    UPDATED

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <geos/geom/GeometryFactory.h>
 using namespace geos::geom;
+#include "temp/temp.h"
 
 int main() {
-  GeometryFactory::Ptr factory = GeometryFactory::create();
-
-
-  
+  GeometryFactory::Ptr factory1 = GeometryFactory::create();
+  GeometryFactory::Ptr factory2 = GeometryFactory::create();
+  GeometryFactory::Ptr factory3 = GeometryFactory::create();
+  GeometryFactory::Ptr factory4 = GeometryFactory::create();
   std::cout << "Hello, world! 2" << std::endl;
+  std::cout << "Hello, world! 3" << std::endl;
+  std::cout << "Hello, world! 4" << std::endl;
+  // std::cout << "Hello, world! 5" << std::endl;
+  AAA aaa;
+  aaa.printHello();
   return 0;
 }
---
+++ src/temp/temp.cpp
@@ -0,0 +1,6 @@
+#include "temp.h"
+#include <iostream>
+
+void AAA::printHello() {
+  std::cout << "Hello ANY aaa" << std::endl;
+}
